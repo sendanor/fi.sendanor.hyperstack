4ef24183de6b35490f7b847a53ad6676057da46b
Leppaax <Pintaleevi@gmail.com>
2023-11-03 15:43:20 +0200

    korjaus

--- dto/HyperViewDTO.test.ts
+++ dto/HyperViewDTO.test.ts
@@ -3,13 +3,22 @@ import {createHyperViewDTO} from "./HyperViewDTO";
 
 describe('createHyperViewDTO', () => {
     it('Should create some viewable data object', () => {
-        const HyperView = createHyperViewDTO("Testi")
+        const HyperView = createHyperViewDTO(
+            "Testi",
+            "Testi",
+            "Testi",
+            "Testi",
+            undefined,
+            "Testi",
+            undefined
+        )
 
         expect(HyperView.name).toBe("Testi");
-        expect(HyperView.extend).toBeUndefined();
-        expect(HyperView.publicUrl).toBeUndefined();
-        expect(HyperView.language).toBeUndefined();
+        expect(HyperView.extend).toBe("Testi");
+        expect(HyperView.publicUrl).toBe("Testi");
+        expect(HyperView.language).toBe("Testi");
         expect(HyperView.seo).toBeUndefined();
+        expect(HyperView.content).toBe("Testi");
         expect(HyperView.style).toBeUndefined();
     });
 });
